a6c03a40ba9446f818a4e222c1b2ab8e3cc86ca6
Shamsiddin Nabiev <shamsiddin@live.ru>
2019-01-28 19:21:58 +0500

    bug fix

--- app/event/action.js
+++ app/event/action.js
@@ -2,7 +2,7 @@ const User = require('../controllers/user')
 
 module.exports = (event, state, map, send) => {
     event.on('progress', async (user, msg, action, next) => {
-        const caption = `\`${locale('name')}👔 : \`*${user.name}*\n\`${locale('job')} 💼 :\`*${user.job}*\n`
+        const caption = `\`${locale('name')} 👔:\`*${user.name}*\n\`${locale('job')}💼:\`*${user.job}*\n`
         await send.photo(user.id, user.photo, caption)
     })
 
