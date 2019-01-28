112081421dfb071c2103a1adb827cf09893ec518
Shamsiddin Nabiev <shamsiddin@live.ru>
2019-01-28 19:19:14 +0500

    changed info message

--- app/event/action.js
+++ app/event/action.js
@@ -2,7 +2,7 @@ const User = require('../controllers/user')
 
 module.exports = (event, state, map, send) => {
     event.on('progress', async (user, msg, action, next) => {
-        const caption = `\`👔${locale('name')}:\`*${user.name}*\n\`💼${locale('job')}:\`${user.job}\n\`♀️♂️${locale('gender')}:\`${locale(user.gender)}\n`
+        const caption = `\`${locale('name')}👔 : \`*${user.name}*\n\`${locale('job')} 💼 :\`*${user.job}*\n`
         await send.photo(user.id, user.photo, caption)
     })
 
