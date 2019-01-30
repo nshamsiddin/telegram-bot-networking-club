a9d4baa13b67d3338120df04e5bb0fca30d5cacd
Shamsiddin Nabiev <shamsiddin@live.ru>
2019-01-30 21:01:14 +0500

    added photoAndKeyboard method

--- modules/telegam.js
+++ modules/telegam.js
@@ -7,7 +7,7 @@ const config = require('../resources/config')
 let bot = null
 
 bot = new TelegramBot(config.bot.token, {
-    polling: true, 
+    polling: true,
     // request: {
     //     proxy: "http://127.0.0.1:15089"
     // },
@@ -106,11 +106,61 @@ exports.keyboard = (user, message, data, inline = 2, options = {}) => {
     })
 }
 
+exports.photoAndKeyboard = (user, file_id, caption, data, inline = 1, options = {}) => {
+    let opt = [], arr = [], i = 0
+
+    // If the map object entered, we take the data from the current branch
+    if (!Array.isArray(data)) {
+        for (let item in data.children) {
+            arr.push(item)
+        }
+    } else {
+        // Received a normal array
+        arr = data
+    }
+
+    for (let key of arr) {
+        if (key === '*') continue
+        // If the inline is greater than 1, then insert the inline elements in one line
+        if (i < inline && opt[opt.length - 1] !== undefined) {
+            opt[opt.length - 1].push({
+                text: emoji.encode(key)
+            })
+        } else {
+            if (i === inline) i = 0
+            opt.push([{ text: emoji.encode(key) }])
+        }
+        i++
+    }
+
+    bot.sendPhoto(user, file_id, {
+        caption: caption,
+        parse_mode: 'Markdown',
+        reply_markup: {
+            keyboard: opt,
+            resize_keyboard: true,
+            // one_time_keyboard: true
+        },
+        ...options
+    })
+
+
+    // bot.sendMessage(user, message, {
+    //     reply_markup: {
+    //         keyboard: opt,
+    //         resize_keyboard: true
+    //         // one_time_keyboard: true
+    //     },
+
+    // })
+}
+
 // Send a message with the keyboard hiding
 exports.messageHiddenKeyboard = (user, message) => {
     bot.sendMessage(user, message, {
         reply_markup: {
-            remove_keyboard: true
-        }
+            remove_keyboard: true,
+        },
+        parse_mode: 'Markdown',
     })
 }
