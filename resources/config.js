c3ddf55d54dc263237365b66d9401e827a5163fa
Shamsiddin <shamsiddin@live.ru>
2019-02-08 17:39:59 +0500

    changed config

--- resources/config.js
+++ resources/config.js
@@ -4,8 +4,8 @@ module.exports = {
     bot: {
         file_api: 'https://api.telegram.org/file/bot', //<token>/<file_path>',
         // token: '293317035:AAGbBVDWTJcfU4KN0Q3NtzEHcS-3w3MvQDg'
-        token: '639456717:AAGXLdFjJO-PG3xys7f9jDAC5UeF9wJBK9s' // Test Networking bot
-        // token: '699149533:AAE2em4IAmze3CZjI3BHNVaIpqA3oj5v9Oc' //Beeline networking bot
+        // token: '639456717:AAGXLdFjJO-PG3xys7f9jDAC5UeF9wJBK9s' // Test Networking bot
+        token: '699149533:AAE2em4IAmze3CZjI3BHNVaIpqA3oj5v9Oc' //Beeline networking bot
     },
     server_ip: '172.30.13.25',
     default_folder: 'cache',
