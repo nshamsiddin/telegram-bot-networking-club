4383553b7a64b300bfef56a7c40c5f005dd000ed
Shamsiddin <shamsiddin@live.ru>
2019-03-01 00:22:26 +0500

    fix bug

--- app/maps/map_register.js
+++ app/maps/map_register.js
@@ -1,65 +1,65 @@
 module.exports = {
     event: 'register:home',
     children: {
-        [locale('activate')]: {
-            event: 'register:activate',
-            children: {
-                '*': {
-                    event: 'register:password',
-                    children: {
-                        [locale('upload_photo')]: {
-                            event: 'register:photo:upload',
-                            children: {
-                                '*': { event: 'register:photo:upload:await', await: true },
-                            }
-                        },
-                        [locale('choose_photo')]: {
-                            event: 'register:photo:choose',
-                            children: {
-                                '*': { event: 'register:photo:choose:await', await: true },
-                                [locale('choose')]: { event: 'registration:complete' }
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        // [locale('register')]: {
-        //     event: 'register:init',
+        // [locale('activate')]: {
+        //     event: 'register:activate',
         //     children: {
         //         '*': {
-        //             event: 'register:username',
+        //             event: 'register:password',
         //             children: {
-        //                 '*': {
-        //                     event: 'register:job',
+        //                 [locale('upload_photo')]: {
+        //                     event: 'register:photo:upload',
         //                     children: {
-        //                         '*': {
-        //                             event: 'register:gender',
-        //                             children: {
-        //                                 [locale('upload_photo')]: {
-        //                                     event: 'register:photo:upload',
-        //                                     children: {
-        //                                         '*': { event: 'register:photo:upload:await', await: true },
-        //                                     }
-        //                                 },
-        //                                 [locale('choose_photo')]: {
-        //                                     event: 'register:photo:choose',
-        //                                     children: {
-        //                                         '*': { event: 'register:photo:choose:await', await: true },
-        //                                         [locale('choose')]: { event: 'registration:complete' }
-        //                                     }
-        //                                 }
-        //                             }
-        //                         },
-
-        //                         // [locale('back')]: { event: 'register:location:back' }
-        //                     },
+        //                         '*': { event: 'register:photo:upload:await', await: true },
+        //                     }
         //                 },
-        //                 // [locale('back')]: { event: 'register:location:back' }
+        //                 [locale('choose_photo')]: {
+        //                     event: 'register:photo:choose',
+        //                     children: {
+        //                         '*': { event: 'register:photo:choose:await', await: true },
+        //                         [locale('choose')]: { event: 'registration:complete' }
+        //                     }
+        //                 }
         //             }
-        //         },
-        //         // [locale('back')]: { event: 'register:location:back' }
+        //         }
         //     }
-        // }
+        // },
+        [locale('register')]: {
+            event: 'register:init',
+            children: {
+                '*': {
+                    event: 'register:username',
+                    children: {
+                        '*': {
+                            event: 'register:job',
+                            children: {
+                                '*': {
+                                    event: 'register:gender',
+                                    children: {
+                                        [locale('upload_photo')]: {
+                                            event: 'register:photo:upload',
+                                            children: {
+                                                '*': { event: 'register:photo:upload:await', await: true },
+                                            }
+                                        },
+                                        [locale('choose_photo')]: {
+                                            event: 'register:photo:choose',
+                                            children: {
+                                                '*': { event: 'register:photo:choose:await', await: true },
+                                                [locale('choose')]: { event: 'registration:complete' }
+                                            }
+                                        }
+                                    }
+                                },
+
+                                // [locale('back')]: { event: 'register:location:back' }
+                            },
+                        },
+                        // [locale('back')]: { event: 'register:location:back' }
+                    }
+                },
+                // [locale('back')]: { event: 'register:location:back' }
+            }
+        }
     }
 }
